cb1489e41a2d7593a2efa7bafe8edcc3ce4b9aa2
Oskar Wróbel <kontakt@oskarwrobel.pl>
2020-01-14 17:53:49 +0100

    Removed utils frapper from tests.

--- test/uid.ts
+++ test/uid.ts
@@ -1,20 +1,18 @@
 import uid from '../src/uid';
 import { expect } from 'chai';
 
-describe( 'uid', () => {
-	it( 'should return different ids', () => {
-		const id1 = uid();
-		const id2 = uid();
-		const id3 = uid();
+it( 'should return different ids', () => {
+	const id1 = uid();
+	const id2 = uid();
+	const id3 = uid();
 
-		expect( id1 ).to.be.a( 'string' );
-		expect( id2 ).to.be.a( 'string' ).to.not.equal( id1 ).to.not.equal( id3 );
-		expect( id3 ).to.be.a( 'string' ).to.not.equal( id1 ).to.not.equal( id2 );
+	expect( id1 ).to.be.a( 'string' );
+	expect( id2 ).to.be.a( 'string' ).to.not.equal( id1 ).to.not.equal( id3 );
+	expect( id3 ).to.be.a( 'string' ).to.not.equal( id1 ).to.not.equal( id2 );
 
-		const uuidRegex = /^e[a-f0-9]{32}$/;
+	const uuidRegex = /^e[a-f0-9]{32}$/;
 
-		expect( id1 ).to.match( uuidRegex );
-		expect( id2 ).to.match( uuidRegex );
-		expect( id3 ).to.match( uuidRegex );
-	} );
+	expect( id1 ).to.match( uuidRegex );
+	expect( id2 ).to.match( uuidRegex );
+	expect( id3 ).to.match( uuidRegex );
 } );
