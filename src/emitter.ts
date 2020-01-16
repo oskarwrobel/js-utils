3e621134db891b35d964f8f842df6580262a8fe7
Oskar Wróbel <kontakt@oskarwrobel.pl>
2020-01-16 19:09:07 +0100

    Added missing Emitter docs.

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,18 +1,49 @@
 export interface EmitterInterface extends Emitter {}
 type Callback = ( ...args: any ) => void;
 
-export default class Emitter {
+/**
+ * Injects Events emitter API into its host.
+ *
+ * @mixin
+ */
+export default class Emitter implements EmitterInterface {
+	/**
+	 * Map of emitters with register callbacks that are subscribed by this instance.
+	 * Used to recognize which callback was registered by this instance.
+	 */
 	private _subscribedEmitters: Map<EmitterInterface, Callback[]> = new Map();
+
+	/**
+	 * Map of events and all registered callbacks under this event.
+	 */
 	private _events: Map<string, Callback[]> = new Map();
 
+	/**
+	 * Registers a callback function to be executed when an event is fired.
+	 *
+	 * @param eventName Name of event.
+	 * @param callback Function executed when event is fired.
+	 */
 	on( eventName: string, callback: Callback ): void {
 		this.listenTo( this, eventName, callback );
 	}
 
+	/**
+	 * Stops executing the callback on the given event.
+	 *
+	 * @param eventName Name of event.
+	 * @param callback Function to stop being called.
+	 */
 	off( eventName: string, callback: Callback ): void {
 		this.stopListening( this, eventName, callback );
 	}
 
+	/**
+	 * Fires an event, executing all callbacks registered for it.
+	 *
+	 * @param eventName Name of event.
+	 * @param args Additional arguments passed to the callback.
+	 */
 	fire( eventName: string, ...args: any[] ): void {
 		if ( !this._events.has( eventName ) ) {
 			return;
@@ -23,6 +54,13 @@ export default class Emitter {
 		}
 	}
 
+	/**
+	 * Registers a callback function to be executed when the given emitter fires an event.
+	 *
+	 * @param emitter Object that fires event.
+	 * @param eventName Name of event.
+	 * @param callback Function executed when event is fired.
+	 */
 	listenTo( emitter: EmitterInterface, eventName: string, callback: Callback ): void {
 		if ( !this._subscribedEmitters.has( emitter ) ) {
 			this._subscribedEmitters.set( emitter, [] );
@@ -39,6 +77,13 @@ export default class Emitter {
 		eventCallbacks.push( callback );
 	}
 
+	/**
+	 * Stops listening for events.
+	 *
+	 * @param emitter Emitter to stop listening to.
+	 * @param eventName Name of event.
+	 * @param callback Function to stop being called.
+	 */
 	stopListening( emitter?: EmitterInterface, eventName?: string, callback?: Callback ): void {
 		if ( !this._subscribedEmitters.size || emitter && !this._subscribedEmitters.has( emitter ) ) {
 			return;
